ff59461b23e9bc71a54731e9b21b2ecab9baef6b
Eugene Koryabkin <eugenewalknut@gmail.com>
2020-07-06 23:04:28 +0300

    replace to BEM folders: all form elements

--- dist/colors.51653f5b.js
+++ dist/colors.51653f5b.js
@@ -199,7 +199,7 @@ module.hot.accept(reloadCSS);
 "use strict";
 
 require("./colors.scss");
-},{"./colors.scss":"colors.scss"}],"../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./colors.scss":"colors.scss"}],"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -227,7 +227,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "5643" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "6735" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -403,5 +403,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","colors.js"], null)
-//# sourceMappingURL=/colors.8414127a.js.map
+},{}]},{},["../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","colors.js"], null)
+//# sourceMappingURL=/colors.51653f5b.js.map
